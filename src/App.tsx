9bb49c8664cbeb6e9bcc3f24749c20592f27e76d
lauraportfolio7 <cerveauxlaura5@gmail.com>
2026-05-01 11:01:44 +0400

    Range les fichiers du redesign abandonné

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import { Routes, Route, useLocation } from 'react-router'
+import { Routes, Route } from 'react-router'
 import { Navigation } from '@/components/Navigation'
 import { Home } from '@/routes/Home'
-import { HomeV2 } from '@/routes/HomeV2'
 
 export function App() {
-  const location = useLocation()
-  const isV2 = location.pathname.startsWith('/nouveau')
-
   return (
     <>
-      {!isV2 && <Navigation />}
+      <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/nouveau" element={<HomeV2 />} />
       </Routes>
     </>
   )
